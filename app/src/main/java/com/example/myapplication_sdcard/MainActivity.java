d78760bd9b16cf0c4505a77e7750bf977981ddb1
SWARNA GOWRI <swarnagowri.b.2019.cse@rajalakshmi.edu.in>
2022-05-10 09:35:06 +0530

    SD Card

--- app/src/main/java/com/example/myapplication_sdcard/MainActivity.java
+++ app/src/main/java/com/example/myapplication_sdcard/MainActivity.java
@@ -3,12 +3,70 @@ package com.example.myapplication_sdcard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btWrite, btRead;
+    EditText etFileName, etStudentName, etCGPA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btWrite = (Button) findViewById(R.id.btWrite);
+        btRead = (Button) findViewById(R.id.btRead);
+        etFileName = (EditText) findViewById(R.id.etFileName);
+        etStudentName = (EditText) findViewById(R.id.etStudentName);
+        etCGPA = (EditText) findViewById(R.id.etCGPA);
+        btWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                writeData();
+            }
+        });
+        btRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                readData();
+            }
+        });
+    }
+    public void writeData() {
+        String data = "";
+        String fileName = etFileName.getText().toString();
+        try {
+            FileOutputStream fos = openFileOutput(fileName, MODE_PRIVATE);
+            data = etStudentName.getText().toString() + "," + etCGPA.getText().toString();
+            fos.write(data.getBytes());
+            fos.close();
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void readData() {
+        int c;
+        String temp = "";
+        String fileName = etFileName.getText().toString();
+        try {
+            FileInputStream fis = openFileInput(fileName);
+            while ((c = fis.read()) != -1) {
+                temp = temp + Character.toString((char) c);
+            }
+            fis.close();
+            String arr[];
+            arr = temp.split(",");
+            etStudentName.setText(arr[0]);
+            etCGPA.setText(arr[1]);
+        } catch(Exception e) {
+        }
+    }
     }
 }
